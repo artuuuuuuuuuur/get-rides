6e95310270510e304704c1a3d1b8e06faf47326f
Artur de Maria <arturdemaria7@gmail.com>
2024-10-03 21:51:06 -0300

    [UPDATE] map styling and location

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,65 @@
-import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
-
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { useState, useEffect } from "react";
+import * as Location from "expo-location";
+import '../../assets/map_styles/dark-style'
+import darkMap from "../../assets/map_styles/dark-style";
+import defaultMap from "../../assets/map_styles/default-style";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 export default function Home() {
+ const colorScheme = useColorScheme();
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting...";
+  let initialRegion = null;
+
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    const { latitude, longitude } = location.coords;
+    text = `Latitude: ${latitude}, Longitude: ${longitude}`;
+
+    initialRegion = {
+      latitude: latitude,
+      longitude: longitude,
+      latitudeDelta: 0.0021488010306938143,
+      longitudeDelta: 0.0010470673441886902,
+    };
+  }
+
   return (
-    <View className="" style={styles.container}>
-      <MapView style={styles.map} />
+  <ThemeProvider value={colorScheme == 'dark' ? DarkTheme : DefaultTheme}>
+  
+    <View style={styles.container}>
+      {initialRegion && (
+        <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        showsUserLocation
+        followsUserLocation
+        initialRegion={initialRegion}
+        customMapStyle={colorScheme === 'dark' ? darkMap : defaultMap}
+        />
+      )}
+      
     </View>
+      </ThemeProvider>
   );
 }
 
@@ -17,5 +71,9 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     position: "absolute",
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: "center",
   },
 });
